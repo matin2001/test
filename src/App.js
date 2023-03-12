4ff7990bf28cc7745990ed0250a7c0e80d603c1c
matin2001 <matin1380day@gmail.com>
2023-03-12 15:22:08 +0330

    File App.js updated.

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
             }}
           />
           <Form.Group className="mb-3 mt-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
+            <Form.Label>email address</Form.Label>
             <Form.Control
               type="email"
               placeholder="Enter email"
